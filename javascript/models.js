0d043f683adcbac9b278688c2ed782fcf2cded8c
George Matter <george.matter.vi@gmail.com>
2015-10-03 01:52:57 -0700

    add knex table creation

--- javascript/models.js
+++ javascript/models.js
@@ -1,4 +1,24 @@
 module.exports = function(bookshelf) {
+  var knex = bookshelf.knex;
+  knex.schema.createTableIfNotExists('people', function(people) {
+    people.increments();
+    people.string('name');
+    people.integer('age');
+    people.date('ageRecorded');
+    people.string('gender');
+    people.binary('photo');
+    people.string('phone');
+    people.string('email');
+    people.string('location');
+  }
+  knex.schema.createTableIfNotExists('donors', function(donors) {
+    donors.increments();
+    donors.string('name');
+    donors.string('phone');
+    donors.string('email');
+    donors.string('location');
+    donors.boolean('facebook');
+  }
   return {
     Person : bookshelf.Model.extend({
       tableName: 'people'
